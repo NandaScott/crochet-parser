55cef52a0a261084ceabba0b90ca81223f393f8f
NandaScott <nanda1123@gmail.com>
2023-05-08 13:36:40 -0400

    Added comments for future me

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,31 @@ interface ExpandedStep extends Pick<NormalizedTokens, 'title' | 'fastenOff'> {
   steps: Array<{ stepNumber: number, endCount: number, instructions: string, expandedInstructions: string[] }>
 }
 
+/**
+ * This function takes the normalized file and parses the written instructions into expanded instructions.
+ * (e.g.) sc, dec, (2sc, dec)x5, sc
+ * 
+ * turns into
+ * 
+ * [
+      'sc',  'dec', 'sc',  'sc',
+      'dec', 'sc',  'sc',  'dec',
+      'sc',  'sc',  'dec', 'sc',
+      'sc',  'dec', 'sc',  'sc',
+      'dec', 'sc'
+    ]
+ * 
+ * Given the complexity of the file below, the comments should be read with the
+ * following instruction for context: sc, dec, (2sc, dec)x5, sc
+ * 
+ * @param file A parsed file with normalized tokens
+ * @returns A parsed file with the expanded steps added
+ */
 function expandMultiplyShorthand(file: NormalizedTokens): ExpandedStep {
   const { steps } = file;
   const expandedSteps = steps.map(({ instructions, stepNumber, endCount }) => {
+    // Since almost all patterns start with a few single crochets in a magic ring,
+    // we can skip the first instruction.
     if (stepNumber === 1) {
       return {
         stepNumber,
@@ -16,28 +38,38 @@ function expandMultiplyShorthand(file: NormalizedTokens): ExpandedStep {
       }
     }
 
-    const [blockRepeat, blockMultiplier] = instructions.match(/(\(.*\)x\d+)/g)?.[0].split('x') ?? [];
-    const blockRepeatClean = blockRepeat.replace('(', '').replace(')', '');
+    // First we need to get the block multiply instructions and expand that e.g. (2sc, dec)x5
+    const [blockRepeat, blockMultiplier] = instructions
+      .match(/(\(.*\)x\d+)/g) // matches text like (2sc, dec)x5
+      ?.[0] // returns the match element
+      .split('x') ?? [] // split the matched string into ['(2sc, dec)', '5']; coalecse into an array for destructuring
 
-    const expanded = new Array<string>(parseInt(blockMultiplier)) // creates array of length equal to the multiplier
-      .fill(blockRepeatClean) // fills the array with the block of repeated instructions
-      .map((val) => val.split(', ')) // splits each instruction into a single instruction, if applicable
+    if (!blockRepeat || !blockMultiplier) throw new Error(`Something went wrong when getting blockRepeat (${blockRepeat}), and blockMultiplier (${blockMultiplier})`)
+
+    const blockRepeatClean = blockRepeat.replace('(', '').replace(')', ''); // '(2sc, dec)' => '2sc, dec'
+
+    const expanded = new Array<string>(parseInt(blockMultiplier)) // creates array of length equal to the multiplier (e.g. 5)
+      .fill(blockRepeatClean) // fills the array with the block of repeated instructions (e.g. '2sc, dec')
+      .map((val) => val.split(', ')) // splits each instruction into a single instruction, if applicable (e.g. ['2sc', 'dec'])
       .reduce((prev, curr) => [...prev, ...curr], []) // flattens into a single array
 
+    // We can now take our expanded value and insert it into our instruction string.
     const insertExpandedIntoInstructions = instructions
-      .replace(/(\(.*\)x\d+)/g, expanded.join(', '))
-      .split(', ')
-      // This block handles the smaller notation for stitch repeats
+      .replace(/(\(.*\)x\d+)/g, expanded.join(', ')) // matches text like (2sc, dec)x5
+      .split(', ') // split the whole line into tokens
+      // This block handles the smaller notation for stitch repeats. (e.g. 2sc)
       .map<string[]>((token) => {
-        const foo = token.split(/([\d]+|[A-Za-z]+)/g).filter(token => token.length > 0);
+        const foo = token
+          .split(/([\d]+|[A-Za-z]+)/g) // splits 2sc into ['2', 'sc']
+          .filter(token => token.length > 0); // Cleans up empty string from the splitting
 
+        // handles a case where there isn't a number for the stitch, so we just add '1' for data consistency.
         if (foo.length === 1) {
           foo.unshift('1')
         }
 
         const [miniMutliplier, miniRepeat] = foo;
-        const bar = new Array(parseInt(miniMutliplier)).fill(miniRepeat)
-        return bar
+        return new Array(parseInt(miniMutliplier)).fill(miniRepeat) // effectively multiplies our stitch by the appropriate amount.
       })
       .reduce((prev, curr) => [...prev, ...curr], []) // flattens into a single array
 
